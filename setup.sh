f8dda928c55124eeb134b9021897334a363c1adb
Rantouan Achmet <rad@simplecode.gr>
2025-05-21 11:05:22 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,19 +2,25 @@
 
 ### # # ###
 
-CONFIG=~/.bashrc
+PATH_RC="$HOME/.bashrc"
 
 ### # # ###
+
+# install
 
 pkg install -y clang
 
 pkg install -y golang
 
-if grep -q '^alias go=' "$CONFIG"; then
-    sed -i 's|^alias go=.*|alias go="CC=/data/data/com.termux/files/usr/bin/arm-linux-androideabi-clang go"|' "$CONFIG"
+# config
+# config : alias
+if grep -q '^alias go=' "$PATH_RC"; then
+    sed -i 's|^alias go=.*|alias go="CC=/data/data/com.termux/files/usr/bin/arm-linux-androideabi-clang go"|' "$PATH_RC"
 else
-    echo 'alias go="CC=/data/data/com.termux/files/usr/bin/arm-linux-androideabi-clang go"' >> "$CONFIG"
+    echo 'alias go="CC=/data/data/com.termux/files/usr/bin/arm-linux-androideabi-clang go"' >> "$PATH_RC"
 fi
+
+### # # ###
 
 source "$CONFIG"
 
